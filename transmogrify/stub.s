cf6d65bc24d07860b587e4717372185511de0df4
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2021-09-20 12:11:38 +0100

    clean up assembly

--- transmogrify/stub.s
+++ transmogrify/stub.s
@@ -9,41 +9,35 @@ assembly_stub_2:
     push %rbp
     mov %rsp, %rbp
     leaq transmogrify_hooks(%rip), %rax
-    add $8, %rax
     /**
     I have no idea what I'm doing but the tests are passing
     */
-    mov (%rax), %rax
+    mov 8(%rax), %rax
     callq *%rax
     leaq transmogrify_returns(%rip), %rax 
-    add $8, %rax
-    mov (%rax), %rax
+    mov 8(%rax), %rax
     jmp *%rax
 assembly_stub_3:
     push %rbp
     mov %rsp, %rbp
     leaq transmogrify_hooks(%rip), %rax
-    add $16, %rax
     /**
     I have no idea what I'm doing but the tests are passing
     */
-    mov (%rax), %rax
+    mov 16(%rax), %rax
     callq *%rax
     leaq transmogrify_returns(%rip), %rax 
-    add $16, %rax
-    mov (%rax), %rax
+    mov 16(%rax), %rax
     jmp *%rax
 assembly_stub_4:
     push %rbp
     mov %rsp, %rbp
     leaq transmogrify_hooks(%rip), %rax
-    add $24, %rax
     /**
     I have no idea what I'm doing but the tests are passing
     */
-    mov (%rax), %rax
+    mov 24(%rax), %rax
     callq *%rax
     leaq transmogrify_returns(%rip), %rax 
-    add $24, %rax
-    mov (%rax), %rax
+    mov 24(%rax), %rax
     jmp *%rax
